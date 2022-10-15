a26008e1a672498635f69ae129f778bb7c9fd0ee
Santiago Benito <bmsanti@hotmail.com>
2022-10-15 18:34:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var menuUL = document.getElementsByClassName("menu");
+var btnToggle = document.getElementsByClassName("btn-toggle");
+
+btnToggle[0].addEventListener("click", function () {
+  if (menuUL[0].style.height) {
+    menuUL[0].style.height = null;
+  } else {
+    menuUL[0].style.setProperty("height", "calc(100vh - 123.6px)");
+  }
+});
+
+var cards = document.querySelectorAll(".card");
+
+[...cards].forEach((card) => {
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
+});
